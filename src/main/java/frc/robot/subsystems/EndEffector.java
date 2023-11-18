c95daf149fd7f932301cbda549aaeca6793bbc77
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2023-11-17 21:42:42 -0700

    preliminary arm code, some endeffector updates

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -4,8 +4,8 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class EndEffector extends SubsystemBase {
-    private final CANSparkMax leftMotor = new CANSparkMax(1, CANSparkMax.MotorType.kBrushed);
-    private final CANSparkMax rightMotor = new CANSparkMax(2, CANSparkMax.MotorType.kBrushed);
+    private final CANSparkMax leftMotor = new CANSparkMax(1, CANSparkMax.MotorType.kBrushed);//filler motor id
+    private final CANSparkMax rightMotor = new CANSparkMax(2, CANSparkMax.MotorType.kBrushed);//filler motor id
 
     //private double targetSpeed;
 
@@ -23,15 +23,24 @@ public class EndEffector extends SubsystemBase {
     }
 
     public void periodic() {
-        //if the driver presses some sort of button
-            startIntake(4);//fille r value for speed
-        //if the driver presses another button
-            startOutput(4);//filler value for speed
+        //if there is no ball in the endeffector
+            this.startIntake(4);//filler value for speed
+        //else if ball is in the endeffector
+            this.brake();
+        //else if the driver presses shoot button
+            this.startOutput(4);//filler value for speed
+
+            //will need to somehow detect when ball enters/leaves, which will be able to make the endeffector brake or startIntake again
     }
 
     public void startIntake(double speed) {
         this.leftMotor.set(speed);
         this.rightMotor.set(speed);
+    }
+
+    public void brake() {
+        this.leftMotor.set(0);
+        this.rightMotor.set(0);
     }
 
     public void startOutput(double speed) {
