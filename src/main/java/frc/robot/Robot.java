3da56c2ec70d34dadf995dc873a00f507472098b
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2023-11-29 18:05:34 -0700

    comt

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.swerve.Gyroscope;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -26,7 +25,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
@@ -50,10 +48,7 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {
-    CommandScheduler.getInstance().cancelAll();
-    m_robotContainer.zeroAllOutputs();
-  }
+  public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
@@ -61,9 +56,12 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    CommandScheduler.getInstance().cancelAll();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    if (m_autonomousCommand != null) CommandScheduler.getInstance().schedule(m_autonomousCommand);
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   /** This function is called periodically during autonomous. */
@@ -72,31 +70,18 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    CommandScheduler.getInstance().cancelAll();
-    m_robotContainer.enableControllers();
-
-
-    
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    System.out.println(Gyroscope.getPigeonInstance().getYaw());
-    try{
-      for (double d : m_robotContainer.getControllerInputs()){
-        System.out.print(d);
-      }
-      System.out.println();
-    } catch (Exception e){
-      System.out.println(e.getMessage());
-    }
-  }
-
-  @Override
-  public void teleopExit(){
-    m_robotContainer.disableControllers();
-  }
+  public void teleopPeriodic() {}
 
   @Override
   public void testInit() {
